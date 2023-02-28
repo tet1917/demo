bcec750b273caebe36753181f6873d4bd509e0fb
tetsu <tetsu0224.soccer@icloud.com>
2023-02-28 20:12:45 +0900

    fizzbazz thema.comit

--- src/main/java/com/example/demo/controller/fizzbazz.java
+++ src/main/java/com/example/demo/controller/fizzbazz.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +18,18 @@ public class fizzbazz {
 
 	@PostMapping("/fizzbazz")
 	public String fb2(@RequestParam("quantity") int quantity, Model model) {
+		List<String> list1 = new ArrayList<>();
 		for (int i = 1; i <= quantity; i++) {
 			if (i % 3 == 0 && i % 5 == 0) {
-				model.addAttribute("list", "fizzbazz");
+				list1.add("fizzbazz");
 			} else if (i % 3 == 0) {
-				model.addAttribute("list", "fizz");
+				list1.add("fizz");
 			} else if (i % 5 == 0) {
-				model.addAttribute("list", "bazz");
+				list1.add("bazz");
 			} else {
-				model.addAttribute("list", i);
+				list1.add(Integer.toString(i));
 			}
+			model.addAttribute("list1", list1);
 		}
 		return "fizzbazz.html";
 	}
