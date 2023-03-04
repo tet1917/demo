f14e9d99e4166f6450dc508460427bfae4edb517
tetsu <tetsu0224.soccer@icloud.com>
2023-03-04 21:19:42 +0900

    ThreeFIveSeven etc

--- src/main/java/com/example/demo/controller/KeisanController.java
+++ src/main/java/com/example/demo/controller/KeisanController.java
@@ -33,26 +33,39 @@ public class KeisanController {
 		return "keisan.html";
 	}
 	
-	@GetMapping("keisan2")
+	@GetMapping("/keisan2")
 	public String keisanGet() {
 		return "keisan.html";
 	}
 	
-	@PostMapping("/keisan2")
-	public String keisanPost(@RequestParam String p1,@RequestParam String p2,Model model) {
+	@PostMapping(value="/keisan2" ,params="tasu")
+	public String keisanTasu(@RequestParam String p1,@RequestParam String p2,Model model) {
 		String kekkaTasu;
-		String kekkaHiku;
-		String kekkaKake;
-		String kekkaWaru;
-		
 		kekkaTasu = keisanService.tasuParam(p1,p2);
-		kekkaHiku = keisanService.hikuParam(p1,p2);
-		kekkaKake = keisanService.kakeParam(p1,p2);
-		kekkaWaru = keisanService.waruParam(p1,p2);
 		model.addAttribute("kekkaTasu", kekkaTasu);
-		model.addAttribute("kekkaHiku", kekkaHiku);
-		model.addAttribute("kekkaKake", kekkaKake);
-		model.addAttribute("kekkaWaru", kekkaWaru);
 		return "keisan.html";
+	}
+	@PostMapping(value="/keisan2" ,params="hiku")
+	public String keisanhiku(@RequestParam String p1 ,@RequestParam String p2,Model model) {
+		String kekkaHiku;
+		kekkaHiku=keisanService.hikuParam(p1,p2);
+		model.addAttribute("kekkaHiku",kekkaHiku);
+		return "keisan.html";
+	}
+	@PostMapping(value="/keisan2",params="kake")
+	public String keisanKake(@RequestParam String p1,@RequestParam String p2 ,Model model) {
+		String kekkaKake;
+		kekkaKake = keisanService.kakeParam(p1,p2);
+		model.addAttribute("kekkaKake",kekkaKake);
+		return "keisan.html";
+		
+	}
+	@PostMapping(value="/keisan2",params="waru")
+	public String keisanWaru(@RequestParam String p1,@RequestParam String p2 ,Model model) {
+		String kekkaWaru;
+		kekkaWaru = keisanService.kakeParam(p1,p2);
+		model.addAttribute("kekkaKake",kekkaWaru);
+		return "keisan.html";
+		
 	}
 }
