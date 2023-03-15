371935945fbc4ea7eae256d2170e6fe5bfb080eb
tetsu <tetsu0224.soccer@icloud.com>
2023-03-15 09:16:34 +0900

    hennsyuu

--- src/main/java/com/example/demo/Service/GetApiServise.java
+++ src/main/java/com/example/demo/Service/GetApiServise.java
@@ -1,7 +1,5 @@
 package com.example.demo.Service;
 
-import java.util.ArrayList;
-
 import org.springframework.stereotype.Service;
 
 import com.example.demo.Repository.GetApiRepository;
@@ -17,24 +15,22 @@ public class GetApiServise {
 		this.getApiRepository = getApiRepository;
 	}
 
-	public ArrayList<String> getApi() throws JsonMappingException, JsonProcessingException {
+//	public ArrayList<String> getApi() throws JsonMappingException, JsonProcessingException {
+//		AddEntity addEntity = new AddEntity();
+//		addEntity = getApiRepository.callMethod();
+//		ArrayList<String> list = new ArrayList<String>();
+//		int plus = addEntity.getData().size();
+//
+//		for (int i = 0; i < plus; i++) {
+//			list.add(addEntity.getData().get(i).getName() +addEntity.getData().get(i).getNote() + addEntity.getData().get(i).getAge().toString() + addEntity.getData().get(i).getRegisterDate());
+//		}
+//		return list;
+	public AddEntity getapi() throws JsonMappingException, JsonProcessingException {
 		AddEntity addEntity = new AddEntity();
 		addEntity = getApiRepository.callMethod();
-		ArrayList<String> list = new ArrayList<String>();
-		int plus = addEntity.getData().size();
-
-		for (int i = 0; i < plus; i++) {
-			list.add(addEntity.getData().get(i).getName() +addEntity.getData().get(i).getNote() + addEntity.getData().get(i).getAge().toString() + addEntity.getData().get(i).getRegisterDate());
-		}
-		return list;
-		//	public String getApi() throws JsonMappingException, JsonProcessingException {
-		// AddEntity addEntity = new AddEntity();
-		// Datum datum = new Datum();
-		// addEntity = getApiRepository.callMethod();
-		// Map<String, String> exMap = new HashMap<>();
-		// exMap.put(AddEntity.getData(), AddEntity.data);
-		// exMap.forEach((key, val) -> (key + " : " + val));
-		// Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
-		// additionalProperties.put(Datum.getName(), Datum.name);
+		return addEntity;
 	}
+
+//
+//	}
 }
--- src/main/java/com/example/demo/controller/GetApiController.java
+++ src/main/java/com/example/demo/controller/GetApiController.java
@@ -1,29 +1,35 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.Service.GetApiServise;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
-@Controller		
+@Controller
 public class GetApiController {
 	private final GetApiServise getApiServise;
+
 	public GetApiController(GetApiServise getApiServise) {
 		this.getApiServise = getApiServise;
-		
 	}
-	
+
 	@GetMapping("/aaa")
-	public String aaa(Model model) throws JsonMappingException, JsonProcessingException {
-		ArrayList<String> list = new ArrayList<>();
-		list=getApiServise.getApi();
-		model.addAttribute("api",list);
+//	public String aaa(Model model) throws JsonMappingException, JsonProcessingException {
+//		ArrayList<String> list = new ArrayList<>();
+//		list = getApiServise.getApi();
+//		model.addAttribute("api", list);
+//		return "GetApi.html";
+//	}
+	public String aaa(){
 		return "GetApi.html";
-		
+	}
+	@PostMapping("/aaa")
+	public String bbb(Model model) throws JsonMappingException, JsonProcessingException {
+		model.addAttribute("key" ,getApiServise.getapi().getData());
+		return "GetApi.html";
 	}
 }
